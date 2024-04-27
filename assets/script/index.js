60b02eb25aa6a06f557e98bae865a16e0e417fe2
dennis1645 <dennis.putra.ti.23@cic.ac.id>
2024-04-27 10:50:15 +0700

    Update Carousel Gambar dan Translate - Dennis

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,15 +2,31 @@
 const bahasa = document.querySelector('#bahasa');
 const en = document.querySelector('#en');
 const id = document.querySelector('#ind');
+const about = document.querySelector('#about');
+const testimoni = document.querySelector('#testimoni');
+const product = document.querySelector('#products');
+const contact = document.querySelector('#contact');
+
+
 
 en.addEventListener('click', () => {
   bahasa.innerHTML = 'english';
+  about.innerHTML = 'about';
+  testimoni.innerHTML = 'testimoni';
+  product.innerHTML = 'products';
+
+  contact.innerHTML = 'contact';
   en.classList.add('hidden');
   id.classList.remove('hidden');
 });
 
 id.addEventListener('click', () => {
   bahasa.innerHTML = 'indonesia';
+  about.innerHTML = 'tentang';
+  testimoni.innerHTML = 'testimoni';
+  product.innerHTML = 'produk';
+
+  contact.innerHTML = 'kontak';
   id.classList.add('hidden');
   en.classList.remove('hidden');
 });
