74e19ae69a235f8c2098d1251cfea666f9dc601b
dennis1645 <dennis.putra.ti.23@cic.ac.id>
2024-04-27 11:07:18 +0700

    Update Carousel Gambar dan Translate PART 2- Dennis

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,10 +6,13 @@ const about = document.querySelector('#about');
 const testimoni = document.querySelector('#testimoni');
 const product = document.querySelector('#products');
 const contact = document.querySelector('#contact');
-
+const judul_hero = document.querySelector('#judul-hero');
+const sub = document.querySelector('#subtitle');
 
 
 en.addEventListener('click', () => {
+  sub.innerHTML = '<p id="subtitle" class="outfit f-24">control what you want to wear<br>be unlimited</p>'
+  judul_hero.innerHTML = '<h1 id="judul-hero" class="judul outfit">MAKE YOUR OWN <br> <span style="font-weight: bold; color: #6B4E4E;" id="fashion">FASHION</span></h1>'
   bahasa.innerHTML = 'english';
   about.innerHTML = 'about';
   testimoni.innerHTML = 'testimoni';
@@ -21,6 +24,8 @@ en.addEventListener('click', () => {
 });
 
 id.addEventListener('click', () => {
+  sub.innerHTML = 'kendalikan apa yang ingin Anda kenakan<br>menjadi tidak terbatas'
+  judul_hero.innerHTML = 'BUATLAH <br> <span style="font-weight: bold; color: #6B4E4E;" id="fashion">FASHION</span> <br>MILIKMU SENDIRI';
   bahasa.innerHTML = 'indonesia';
   about.innerHTML = 'tentang';
   testimoni.innerHTML = 'testimoni';
